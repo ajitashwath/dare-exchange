f9f65d9668a6a26e07df814406ba44609424b6ba
ajitashwath <ajitashwathr23@lpu.in>
2025-08-01 19:18:10 +0530

    updated

--- daredb/settings.py
+++ daredb/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 import os
 import sys
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -93,10 +94,9 @@ WSGI_APPLICATION = 'daredb.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 
